39a57ee168321c5b54d5633d23a869670e80c967
angemurchi <39175599+angemurchi@users.noreply.github.com>
2018-09-30 22:53:30 -0400

    showing figures

--- crop-and-tile-1.py
+++ crop-and-tile-1.py
@@ -8,17 +8,28 @@ Created on Sun Sep 30 21:53:37 2018
 import os
 from skimage import io, filters
 import numpy as np
+import matplotlib.pyplot as plt
 import time
+#io.use_plugin('matplotlib')
 
 directory = r'C:\Users\angem\Desktop\VG 3D - R2\01052016 week 2'
-print('The current directory is {}'.format(os.path.realpath(os.curdir)))
+print(                                                                         #Print prints strings to the console
+      'The current directory is {}'.format(os.path.realpath(os.curdir)) #the format method (function) for strings inserts the input argument in teh string at the curly brackets. You can insert many arguments
+      )
 os.chdir(directory)
 print('The current directory has been changed to {}'.format(os.path.realpath(os.curdir)))
 for k,fname in enumerate(os.listdir()):
-    if fname.rsplit('.')[-1]:
+    if fname.rsplit('.')[-1]: #rsplit splits the string into separate strings at the given delimiter
         print('Reading file {}'.format(fname))
-        im,a = io.imread(fname)
+        im= io.imread(fname)
+        plt.figure((k+1)*1000)
+        print('Show pictures as they were read in')
         io.imshow(im)
-        time.sleep(1)
+        print('now lets show each color channel')
+        for k2 in np.arange(np.size(im,axis=2)):
+            plt.figure(k*1000+k2+1)
+            io.imshow(im[k2])
+        
+        #time.sleep(1)
     if k >=1:
         break
