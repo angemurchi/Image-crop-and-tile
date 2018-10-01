ace29251160a8f87518d6a87c8c5865a3c0dc93e
angemurchi <39175599+angemurchi@users.noreply.github.com>
2018-09-30 22:27:41 -0400

    added loop to read images from directory

--- crop-and-tile-1.py
+++ crop-and-tile-1.py
@@ -5,3 +5,20 @@ Created on Sun Sep 30 21:53:37 2018
 @author: angem
 """
 
+import os
+from skimage import io, filters
+import numpy as np
+import time
+
+directory = r'C:\Users\angem\Desktop\VG 3D - R2\01052016 week 2'
+print('The current directory is {}'.format(os.path.realpath(os.curdir)))
+os.chdir(directory)
+print('The current directory has been changed to {}'.format(os.path.realpath(os.curdir)))
+for k,fname in enumerate(os.listdir()):
+    if fname.rsplit('.')[-1]:
+        print('Reading file {}'.format(fname))
+        im,a = io.imread(fname)
+        io.imshow(im)
+        time.sleep(1)
+    if k >=1:
+        break
